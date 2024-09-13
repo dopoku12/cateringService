d4bd3778d59f3066905ca7ebef566e8070f00e3e
dopoku12 <dopoku.tech@gmail.com>
2024-09-13 05:04:55 -0400

    theme provider added

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,43 @@
 
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
+const homeImg='src/assets/desscEdit.webp'
 const Home = () => {
-   return <>
-   
-   </>;
-};
+   return (<>
+   <img src={homeImg} width='100%' height='50%' alt="" />
+<Box>
+
+<Typography variant="h5" sx={{fontWeight:'bold'}}>
+   Mission
+
+</Typography>
+  <Typography variant='subtitle2'>
+     Provide quality full-service catering that meets your every need and exceeds your expectation.
+   </Typography>
+</Box>
+<Box>
+
+<Typography variant="h5" sx={{fontWeight:'bold'}}>
+   Vision
+
+</Typography>
+  <Typography variant='subtitle2'>
+    Returning Home-Style cooking back into catering by creating nutritious, delicious and classic menus
+   </Typography>
+</Box>
+<Box>
+
+<Typography variant="h5" sx={{fontWeight:'bold'}}>
+   Our Commitment
+   </Typography>   
+   <Typography variant='subtitle2'>
+    Locally sourced produce and the freshest ingredients mean﻿s food
+
+that is exceptional in taste.
+   </Typography>
+</Box>
+</>
+)};
 
 export default Home;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,37 +8,54 @@ import {
    ListItemText,
    Stack,
    Typography,
-   
-} from "@mui/material";
-import '@fontsource/pacifico/400.css'
+   Tooltip,
+   Link as MuiLink 
 
-const Nav = () => {
+} from "@mui/material";
+import PhoneIcon from "@mui/icons-material/Phone";
+
+
 const logo = `src/assets/dees.webp`;
-   const listItems = [
-      { name: "Home", url: "/" },
-      { name: "Menu", url: "/menu" },
-      { name: "Services", url: "/services" },
-      {name: "About Us",url: "/aboutUs",},
+
+const listItems = [
+   { name: "Home", url: "/" },
+   { name: "Menu", url: "/menu" },
+   { name: "Services", url: "/services" },
+   {name: "About",url: "/aboutUs",},
    ].map((value, i) => (
-                  <Link key={i} style={{textDecoration:'none',color:'black'}} to={value.url}>
-               <Button variant="text">
+                  <Link  key={i} style={{textDecoration:'none',color:'black'}} to={value.url}>
+               <Button size="small" variant="text" color="warning" sx={{color:'ButtonText'}}>
+                  <Typography  fontWeight='bold' fontSize='12px'>
                   {value.name}
+                  </Typography>
                </Button>
                   </Link>
    ));
-
+   
+   const Nav = () => {
    return (
       <Box component="nav" >
+             <Stack spacing={2} direction='row' marginBottom={6}>
+                  {listItems}
+                   <Tooltip title="call us!">
+                  <MuiLink href="tel:+1 301-675-3848">
+                     <PhoneIcon  color="warning"/>
+                  </MuiLink>
+               </Tooltip>
+            </Stack>
          <AppBar sx={{ bgcolor: "transparent", boxShadow:'none',position:'static',}}>
-              <Box component="div" sx={{display:'flex', flexDirection:'column',justifyContent:'center', alignItems:'center'}} >
+              <Box component="div" sx={{display:'flex', flexDirection:'column',justifyContent:'space-between', alignItems:'center'}} >
                   <Link  to="/">
                      <img style={{borderRadius:'50%'}} width={60} src={logo} alt="logo" />
                   </Link>
-               <Typography gutterBottom variant='h5' sx={{ fontFamily: 'pacifico,cursive,arial',color:'black',}} >Dees Catering Services</Typography>
+               <Typography  variant='h5' sx={{ fontFamily: 'pacifico,cursive,arial',color:'black', marginBottom:3}} >
+                  Dees Catering Services
+                  </Typography>
+                  <Typography variant="subtitle2" sx={{  marginBottom:4}}>
+                     Redefining Food & Event Production
+                  </Typography>
                </Box>
-                 <Stack spacing={2} direction='row'>
-                  {listItems}
-            </Stack>
+
          </AppBar>
       </Box>
    );
